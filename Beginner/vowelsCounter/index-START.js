d946eabb260fb5afc1b677a9d2688fa837991b63
merrickcho <merrick.cho@gmail.com>
2022-11-06 03:07:21 -0800

    Completed vowelCounter

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,26 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+//# Using iterative
+// const vowels = ['a','e','i','o','u']
+// function vowelsCounter(text) {
+//     let count =0
+//     for(let letter of text.toLowerCase()){
+//         if(vowels.includes(letter)){
+//             count++
+//         }
+//     }
+//     return count
+// }
 
+//# Using Regex
 function vowelsCounter(text) {
-    // Code goes here
+    let res = text.match(/[aeiou]/gi)
+    if (res){
+        return res.length
+    }else{
+        return 0
+    }
 }
-
-
 
 module.exports = vowelsCounter;
